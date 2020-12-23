a910a681854dd8911e993f913100c79c1865db82
Aries <aho11@stuy.edu>
2020-12-23 14:26:14 -0500

    MakeStars is working to censor

--- MakeStars.java
+++ MakeStars.java
@@ -6,6 +6,16 @@ public class MakeStars{
       Scanner n = new Scanner( System.in );
       //use hasNextLine()/nextLine() to loop over
       //all of the data
+      String translate = "";
+      while(n.hasNextLine()){
+        translate = n.nextLine();
+        String ns = "";
+        for(int i = 0; i < translate.length(); i++){
+          if(translate.charAt(i) != ' ') ns = ns + "*";
+          else ns = ns + " ";
+        }
+        System.out.println(ns);
+      }
 
       //If you want to read the input word by word
       //this can be replaced with hasNext() and next()
